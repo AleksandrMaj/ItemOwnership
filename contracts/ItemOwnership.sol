9eec6c79efd44bfcb0e6e247e523d8dd6043db40
AleksandrMaj <aleksandrmaj2001@gmail.com>
2023-06-23 19:05:48 +0200

    Feature: implement method to get attributes of an item

--- contracts/ItemOwnership.sol
+++ contracts/ItemOwnership.sol
@@ -21,6 +21,7 @@ contract ItemOwnership is ContractOwnable {
     struct Item {
         address owner;
         mapping(string => string) attributes;
+        string[] attributeKeys;
     }
     
     constructor() {
@@ -32,14 +33,21 @@ contract ItemOwnership is ContractOwnable {
 
         Item storage newItem = itemDatabase[msg.sender][_id];
         newItem.owner = msg.sender;
-        newItem.attributes["key1"] = "value1";
-
-        // itemDatabase[msg.sender][_id];
+        newItem.attributes["key1"] = "value1"; //TODO: Be able to determine own attributes
+        newItem.attributes["key2"] = "value2";
+        newItem.attributeKeys.push("key1");
+        newItem.attributeKeys.push("key2");
     }
 
-    // function test(uint256 _id) public view returns(Item item) { //TODO: Check how I can get the attributes
-    //     return itemDatabase[msg.sender][_id];
-    // }
+    function getAttributesOfItemByID(uint256 _id) public view returns(string[] memory, string[] memory) {
+        Item storage item = itemDatabase[msg.sender][_id];
+        string[] memory attributeValues = new string[](item.attributeKeys.length);
+
+        for(uint256 i = 0; i < item.attributeKeys.length; i++) {
+            attributeValues[i] = item.attributes[item.attributeKeys[i]];
+        }
+        return (item.attributeKeys, attributeValues);
+    }
 
     /*function removeItemFromAddress() public {
 
